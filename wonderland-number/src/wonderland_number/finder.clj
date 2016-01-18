f6912dfae4223baefb49291485d09dae96c1a89e
mangosmootie <mangosmoothie@users.noreply.github.com>
2016-01-18 10:03:49 -0600

    magic number generator

--- wonderland-number/src/wonderland_number/finder.clj
+++ wonderland-number/src/wonderland_number/finder.clj
@@ -1,5 +1,20 @@
 (ns wonderland-number.finder)
 
+
+(def wl-range
+  (range 100000 1000000))
+
+(defn have-same-nums
+  [coll]
+  (let [s1 (set (str (first coll)))
+        c (map #(set (str %)) (rest coll))]
+    (every? #(= s1 %) c)))
+
+(defn make-nums
+  [n]
+  (map #(* n %) (range 1 7)))
+
 (defn wonderland-number []
   ;; calculate me
-  42)
+  (first (filter #(have-same-nums (make-nums %)) wl-range)))
+
